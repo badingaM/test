bf108215434a9d7ea2fb54491d843cf55eaf5027
musshca <badinga.musshca-luclaure@ecole.ensicaen.fr>
2020-06-17 19:56:09 +0200

    add gitignore

--- Test for git/test.c
+++ Test for git/test.c
@@ -5,3 +5,14 @@ int main(void){
 
 	return 0; 
 }
+
+/* Git : 
+after installation : 
+git init -> initialise the rep
+git add name file 
+git add .-> add all files 
+ commands unix are available
+
+ git checkout -- . // come back to previous state 
+git commit 
+*/
